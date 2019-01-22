d3a8d1c45f6c9a2776215aa7a113dae3e8f0543d
nona <iKonnyaku40@yahoo.co.jp>
2019-01-22 13:50:21 -0500

    Initial Commit

---
+++ openGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL
+//
+//  Created by Keita Nonaka on 1/22/19.
+//  Copyright © 2019 Keita Nonaka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
