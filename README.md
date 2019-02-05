a5982f739e4d11b90651a6ce0edaa70dacb80ec7
nona <iKonnyaku40@yahoo.co.jp>
2019-02-05 13:49:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# openGL
+
+## Description
+This is for Computer Graphics, CS330.
+
+openGL can be setup through Xcode.
+
+## How to Run
+run main.cpp <- You may change the file
+
+compile: `gcc -framework GLUT -framework OpenGL main.cpp`
+
+run: `./a.out`
--- openGL/main.cpp
+++ openGL/main.cpp
@@ -6,10 +6,40 @@
 //  Copyright © 2019 Keita Nonaka. All rights reserved.
 //
 
-#include <iostream>
+#include "Angel.h"
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+
+void init( void ) {
+  // CHANGE the CLEAR color below (RGB)
+  
+  glClearColor( 1.0, 0.0, 0.0, 1.0 ); // white background
+}
+
+void display( void ) {
+  glPointSize(5);
+  glClear( GL_COLOR_BUFFER_BIT );     // clear the window
+  glFlush();
+}
+
+int main( int argc, char **argv ) {
+  glutInit( &argc, argv );
+  glutInitDisplayMode( GLUT_RGBA );
+  glutInitWindowSize( 512, 512 );
+  
+  // If you are using freeglut, the next two lines will check if
+  // the code is truly 3.2. Otherwise, comment them out
+  
+  //glutInitContextVersion( 3, 2 );
+  //glutInitContextProfile( GLUT_CORE_PROFILE );
+  
+  glutCreateWindow( "Lab00 No Shader" );
+  
+//  glewInit(); // this doesnt work on Xcode
+  
+  init();
+  
+  glutDisplayFunc( display );
+  
+  glutMainLoop();
   return 0;
 }
